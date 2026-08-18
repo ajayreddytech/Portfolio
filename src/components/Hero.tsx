79d5b450ed39f064a37463e5a55517a6b0b6e7f4
Ajay <ajayreddy@gmail.com>
2026-08-18 15:30:28 -0400

    updated resume

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -101,9 +101,9 @@ const Hero = () => {
               onClick={() => {
                 const link = document.createElement("a");
                 link.href =
-                  "https://drive.google.com/file/d/1jhPCLX34CUudtPAiZ5lRKsgWvBSuc8o9/view?usp=drive_link"; // replace with your file URL
+                  "https://drive.google.com/file/d/1oDbiEjX7YD9YvhZUMz1g8pBCcJYf_F15/view?usp=sharing"; // replace with your file URL
                 link.target = "_blank";
-                link.download = "Ajay_Reddy_Full Stack Software Developer_Resume.pdf"; // optional: specify the filename
+                link.download = "Ajay_Reddy_FullStackSoftwareDeveloper_Resume.pdf"; // optional: specify the filename
                 link.click();
               }}
             >
